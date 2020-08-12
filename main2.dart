43c6bec6afb28217ae400722dda5ced795631f56
Ashok M <haitwoashok@gmail.com>
2020-08-12 15:56:47 +0530

    Create main2.dart

---
+++ main2.dart
@@ -0,0 +1,15 @@
+Container(
+  width: 100,
+  height: 100,
+  color: Colors.orange,
+  child: Center(
+    child: Text(
+      '1',
+      style: TextStyle(
+        fontSize: 50,
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  ),
+),
